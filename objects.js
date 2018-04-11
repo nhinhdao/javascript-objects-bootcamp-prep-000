6374daa205a2b4a5799e2daa8adbcfd0b5d14c72
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-11 22:20:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,11 +3,11 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-   var newlist = Object.assign({}, playlist, {artistName: songTitle});
+   var newlist = Object.assign({}, playlist, {[artistName]: songTitle});
    return newlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
-   delete playlist.artistName;
+   delete playlist.[artistName];
    return playlist;
 }
